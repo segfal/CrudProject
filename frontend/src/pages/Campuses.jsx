42be743ad88c055dbcdf612774f6251aaff6860c
Huda Ayaz <hudaayaz21@gmail.com>
2023-06-29 14:36:15 -0400

    Create campus items to display database entries from dummy data

--- frontend/src/pages/Campuses.jsx
+++ frontend/src/pages/Campuses.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import SingleCampus from "./SingleCampus";
 import dummyData from "./schools.json";
+import CampusItems from "../components/CampusItems";
 
 const Campuses = () => {
     return (
         <div>
             <h1>Welcome to the Campuses page</h1>
             <button type="button" class="btn btn-primary">Add Campus</button>
-            {dummyData.map}
+            <CampusItems list={dummyData}/>
             <SingleCampus/>
         </div>
     )
